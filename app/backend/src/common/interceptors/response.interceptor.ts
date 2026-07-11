8f6f9d9b412d8cf3bafd9ad9d751798b3ae905d3
Nuona Liang <lnuo026@aucklanduni.ac.nz>
2026-07-12 01:25:48 +1200

    feat: interceptor+ main

---
+++ app/backend/src/common/interceptors/response.interceptor.ts
@@ -0,0 +1,15 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class ResponseInterceptor<T> implements NestInterceptor {
+     intercept(context: ExecutionContext, next: CallHandler): Observable<unknown> {
+          return next.handle().pipe(
+               map((data: unknown) => ({
+                    data,
+                    message: 'ok',
+               })),
+          );
+     }
+}
--- app/backend/src/main.ts
+++ app/backend/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { WinstonModule } from 'nest-winston';
+import { winstonConfig } from './common/logger/winston.config';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger(winstonConfig),
+  });
+
+  app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalFilters(new AllExceptionsFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    })
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
