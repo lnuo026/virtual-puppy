c1b1873f4eef889b8b46048a63f11533dfd610d8
Nuona Liang <lnuo026@aucklanduni.ac.nz>
2026-07-12 01:07:51 +1200

    feat: exception

---
+++ app/backend/src/common/filters/all-exceptions.filter.ts
@@ -0,0 +1,26 @@
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from "@nestjs/common";
+import { request, Response } from "express";
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+     catch(exception: unknown, host: ArgumentsHost) {
+          const ctx = host.switchToHttp();
+          const response = ctx.getResponse<Response>();
+
+          const isHttpException = exception instanceof HttpException;
+          const status = isHttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
+
+          const exceptionResponse = isHttpException ? exception.getResponse() : null;
+          const message = typeof exceptionResponse === 'string' ? exceptionResponse : (exceptionResponse as { message?: string | string[]})?.message || 'Internal server error';
+
+          response.status(status).json({
+               statusCode: status,
+               error: isHttpException ? exception.constructor.name: 'InternalServerError',
+               message,
+               timestap: new Date().toDateString(),
+               path: request.url,
+          })
+
+     }
+}
+  
