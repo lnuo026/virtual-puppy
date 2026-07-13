a21ddefe17eca38dc5056fd08fb6489833336722
Nuona Liang <lnuo026@aucklanduni.ac.nz>
2026-07-14 00:23:30 +1200

    feat:pet~

---
+++ app/backend/src/modules/pet/lib/constants.ts
@@ -0,0 +1,9 @@
+export const BREEDS = ['shiba', 'corgi', 'poddle', 'husky', 'labrador', 'golden retriever', 'beagle', 'bulldog', 'dalmatian', 'rottweiler', 'german shepherd', 'pug', 'chihuahua', 'boxer', 'dachshund', 'siberian husky', 'border collie', 'australian shepherd', 'great dane', 'cocker spaniel']as const;
+export const COATS = ['brown', 'white', 'black', 'spotted', 'golden'] as const; 
+export const PERSONALITIES = ['friendly', 'playful', 'loyal', 'protective', 'intelligent', 'curious', 'affectionate', 'independent', 'energetic', 'gentle', 'clingy', 'playful'] as const;
+
+export const STARTING_HEALTH = 100;
+
+export const DECAT_PET_MINUTE = {
+     
+}
