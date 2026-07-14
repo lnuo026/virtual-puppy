8122f8242fc8440c0e6a1db696b9639aa626f60f
Nuona Liang <lnuo026@aucklanduni.ac.nz>
2026-07-14 17:24:09 +1200

    module pet

---
+++ app/backend/src/modules/pet/pet.module.ts
@@ -0,0 +1,14 @@
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { PetController } from "./pet.controller";
+import { PetService } from "./pet.service";
+import { Pet, PetSchema } from "./schemas/pet.schema";
+
+@Module({
+     imports: [MongooseModule.forFeature([{ name: Pet.name, schema: PetSchema }])],
+     controllers: [PetController],
+     providers: [PetService],
+     export: [PetService],
+})
+
+export class PetModule {}
