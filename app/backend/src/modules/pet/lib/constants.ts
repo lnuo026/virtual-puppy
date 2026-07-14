a9b9b958bfad79acc3a2d7aa527676c67fad53b6
Nuona Liang <lnuo026@aucklanduni.ac.nz>
2026-07-14 14:56:21 +1200

    feat:petschema

--- app/backend/src/modules/pet/lib/constants.ts
+++ app/backend/src/modules/pet/lib/constants.ts
@@ -1,25 +1,24 @@
-import { hasUncaughtExceptionCaptureCallback } from "process";
 
 export const BREEDS = ['shiba', 'corgi', 'poddle', 'husky', 'labrador', 'golden retriever', 'beagle', 'bulldog', 'dalmatian', 'rottweiler', 'german shepherd', 'pug', 'chihuahua', 'boxer', 'dachshund', 'siberian husky', 'border collie', 'australian shepherd', 'great dane', 'cocker spaniel']as const;
 export const COATS = ['brown', 'white', 'black', 'spotted', 'golden'] as const; 
 export const PERSONALITIES = ['friendly', 'playful', 'loyal', 'protective', 'intelligent', 'curious', 'affectionate', 'independent', 'energetic', 'gentle', 'clingy', 'playful'] as const;
 
 export const STARTING_HEALTH = 100;
+export const STARTING_STAT = 80;
 
-export const DECAT_PET_MINUTE = {
+export const DECAY_PET_MINUTE = {
      hunger: 1,
      thirst: 1,
-     health: 1,
      mood: 0.5,
      energy: 0.3,
      hygiene: 0.4,
 };
 
-export const HUNGER_THRESHOLD = 30;
+export const HUNGRY_THRESHOLD = 30;
 export const TIRED_THRESHOLD = 25;
 export const HAPPY_THRESHOLD = 45;
-export const SICK_ENTER_THRESHOLD = 20;
-export const SICK_EXIT_HEALTH_THRESHOLD = 50;
+export const SICK_ENTER_HEALTH = 20;
+export const SICK_EXIT_HEALTH = 50;
 export const HEALTH_PENALTY_PER_MINUTE = 0.15;
 export const HEALTH_RECOVERY_PER_MINUTE = 0.1;
 
@@ -30,6 +29,6 @@ export const PLAY_ENERGY_COST = 15;
 export const BATH_AMOUNT = 100;
 export const SLEEP_DURATION_MS = 20 * 60 * 1000; // 20 minutes in milliseconds
 
-export const DAILY_CHECK_BONUS =10;
+export const DAILY_CHECKIN_BONUS =10;
 export const DAILY_TASK_BONUS = 10;
 
---
+++ app/backend/src/modules/pet/schema/pet.schema.ts
@@ -0,0 +1,75 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument, Types } from "mongoose";
+import { BREEDS, COATS, PERSONALITIES, STARTING_HEALTH,STARTING_STAT } from "../lib/constants";
+
+export type Breed = (typeof BREEDS)[number];
+export type Coat = (typeof COATS)[number];
+export type Personality = (typeof PERSONALITIES)[number];
+export type PetStatus = 'idle' | 'sad' | 'angry'| 'hunger'| 'tired'| 'happy'| 'sick'| 'sleeping';
+
+export type PetDocument = HydratedDocument<Pet>;
+
+@Schema({timestamps: true})
+export class Pet {
+     @Prop({ type: Types.ObjectId, ref: 'User', required: true, unique: true, index:true })
+     userId!: Types.ObjectId;
+
+     @Prop({ required: true})
+     name!: string;
+
+     
+     @Prop({ required: true, enum: COATS })
+     coat!: Coat;
+     
+     @Prop({ required: true, enum: PERSONALITIES })
+     personality!: Personality;
+     
+     @Prop({ required: true, enum: BREEDS })
+     breed!: Breed;
+     
+     @Prop({ default: STARTING_HEALTH, min: 0, max: 100 })
+     health!: number;
+     
+     status!: PetStatus;
+
+     @Prop({ default: STARTING_STAT, min: 0, max: 100 })
+     hunger!: number;
+
+     @Prop({ default: STARTING_STAT, min: 0, max: 100 })
+     thirst!: number;
+
+     @Prop({ default: STARTING_STAT, min: 0, max: 100 })
+     mood!: number;
+
+     @Prop({ default: STARTING_STAT, min: 0, max: 100 })
+     energy!: number;
+
+     @Prop({ default: STARTING_STAT, min: 0, max: 100 })
+     hygiene!: number;
+
+     @Prop({ default: Date.now })
+     lastInteractionAt!: Date;
+
+     @Prop({ default: Date.now })
+     lastVisitAt!: Date;
+
+     @Prop({})
+     sleepUntil?: Date;
+
+     @Prop({ default: 0 })
+     streakCount!: number;
+
+     @Prop({})
+     lastCheckInDate?: string;
+
+     @Prop({ default: false })
+     dailyFedToday!: boolean;
+
+     @Prop({ default: false })
+     dailyPlayedToday!: boolean;
+
+     @Prop({ default: false })
+     dailyTaskCompletedToday!: boolean;
+}
+
+export const PetSchema = SchemaFactory.createForClass(Pet)
