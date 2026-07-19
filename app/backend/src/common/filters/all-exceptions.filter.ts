0d7730f8083a2668da2645097172d9ebc18f20cc
Nuona Liang <lnuo026@aucklanduni.ac.nz>
2026-07-19 22:55:30 +1200

    fix : exception

--- app/backend/src/common/filters/all-exceptions.filter.ts
+++ app/backend/src/common/filters/all-exceptions.filter.ts
@@ -6,12 +6,15 @@ export class AllExceptionsFilter implements ExceptionFilter {
      catch(exception: unknown, host: ArgumentsHost) {
           const ctx = host.switchToHttp();
           const response = ctx.getResponse<Response>();
+          const request = ctx.getRequest<Request>();
 
           const isHttpException = exception instanceof HttpException;
           const status = isHttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
 
           const exceptionResponse = isHttpException ? exception.getResponse() : null;
           const message = typeof exceptionResponse === 'string' ? exceptionResponse : (exceptionResponse as { message?: string | string[]})?.message || 'Internal server error';
+
+          console.error(`Unhandled execption: `, exception );
 
           response.status(status).json({
                statusCode: status,
