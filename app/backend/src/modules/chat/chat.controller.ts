e4bb1fc68b584542e4f4390565ad87e677792808
Nuona Liang <lnuo026@aucklanduni.ac.nz>
2026-08-11 23:59:31 +1200

    ThrottlerGuard test

--- app/backend/src/modules/chat/chat.controller.ts
+++ app/backend/src/modules/chat/chat.controller.ts
@@ -3,11 +3,13 @@ import { ChatService } from './chat.service';
 import { ChatRequestDto } from './dto/chat-request.dto';
 import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 import type { UserDocument } from '../user/schemas/user.schema';
+import { Throttle } from '@nestjs/throttler';
 
 @Controller('chat')
 export class ChatController {
      constructor(private readonly chatService: ChatService) {}
 
+     @Throttle({ default: { limit: 5, ttl: 60_000 } }) 
      @Post()
      reply(@CurrentUser()user: UserDocument, @Body() dto: ChatRequestDto) {
           return this.chatService.reply(user._id.toString(), dto);
